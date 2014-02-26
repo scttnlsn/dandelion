8ccded88988561406b2ac1a3a127a82a2625f9f1
Scott Nelson <scott@scttnlsn.com>
2014-02-26 08:52:43 -0500

    Added base adapter

--- lib/dandelion.rb
+++ lib/dandelion.rb
@@ -1,4 +1,5 @@
 require 'rugged'
+require 'dandelion/adapter'
 require 'dandelion/diff'
 require 'dandelion/deployer'
 require 'dandelion/workspace'
---
+++ lib/dandelion/adapter.rb
@@ -0,0 +1,42 @@
+module Dandelion
+  module Adapter
+    class MissingDependencyError < StandardError
+      attr_reader :gems
+
+      def initialize(gems)
+        @gems = gems
+      end
+    end
+
+    class Base
+      class << self
+        @@adapters = {}
+
+        def adapter(name)
+          @@adapters[name] = self
+        end
+
+        def create_adapter(name, options = {})
+          klass = @@adapters[name]
+          return nil if klass.nil?
+
+          begin
+            klass.new(options)
+          rescue LoadError
+            raise MissingDependencyError.new(klass.required_gems)
+          end
+        end
+
+        attr_reader :required_gems
+
+        def requires_gems(*gems)
+          @required_gems = gems
+        end
+      end
+
+      def initialize(options)
+        @options = options
+      end
+    end
+  end
+end
